6c9bb7b4a0309225809838483e425bb2129e8fcb
MartinValentinov <159445059+MartinValentinov@users.noreply.github.com>
2025-03-20 19:25:42 +0200

    login

---
+++ login/HackTues/src/main/java/com/site/HackTues/User.java
@@ -0,0 +1,63 @@
+package com.site.HackTues;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String fname;
+
+    @Column(nullable = false)
+    private String lname;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String dob;
+
+    @Column(nullable = false)
+    private String gender;
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getFname() { return fname; }
+    public void setFname(String fname) { this.fname = fname; }
+
+    public String getLname() { return lname; }
+    public void setLname(String lname) { this.lname = lname; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
+
+    public String getDob() { return dob; }
+    public void setDob(String dob) { this.dob = dob; }
+
+    public String getGender() { return gender; }
+    public void setGender(String gender) { this.gender = gender; }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", fname='" + fname + '\'' +
+                ", lname='" + lname + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", dob='" + dob + '\'' +
+                ", gender='" + gender + '\'' +
+                '}';
+    }
+}
