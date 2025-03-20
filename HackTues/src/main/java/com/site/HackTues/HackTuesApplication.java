1cb1aa2645cfd1a1868a2651bb2ad9a6485bfe52
MartinValentinov <159445059+MartinValentinov@users.noreply.github.com>
2025-03-20 09:57:09 +0200

    begin

---
+++ HackTues/src/main/java/com/site/HackTues/HackTuesApplication.java
@@ -0,0 +1,13 @@
+package com.site.HackTues;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackTuesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackTuesApplication.class, args);
+	}
+
+}
